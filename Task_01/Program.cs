04ada763d1fcb794428e4b9895a5889918315763
Unseed <unseedevil@gmail.com>
2023-01-05 00:29:08 +0300

    Решение домашки

---
+++ Task_01/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+
+int length = ReadInt("Array length");
+int[] array = GetArray(length, -100, 100);
+
+PrintArray(array);
+System.Console.WriteLine();
+Search (array);
+
+
+int ReadInt (string argument)
+{
+    System.Console.Write($"Input {argument}: ");
+    return int.Parse(Console.ReadLine());
+}
+
+int[] GetArray(int length, int minValue, int maxValue)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Input number {i}: ");
+        array[i] = int.Parse(Console.ReadLine());
+    }
+   
+    return array;
+}
+
+void PrintArray (int[] array)
+{
+    for (int i= 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}, ");
+    }
+}
+
+void Search (int[] array)
+{
+    int N = 0;
+    for (int i= 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            N += 1;
+        }        
+    }System.Console.WriteLine($"number of positive = {N} ");
+}
---
+++ Task_02/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double b1 = ReadInt("b1 ");
+double k1 = ReadInt("k1 ");
+double b2 = ReadInt("b2 ");
+double k2 = ReadInt("k2 ");
+
+Cross( b1,  k1,  b2,  k2);
+
+double ReadInt (string argument)
+{
+    System.Console.Write($"Input {argument}: ");
+    return int.Parse(Console.ReadLine());
+}
+void Cross(double b1, double k1, double b2, double k2)
+{
+    double x = 0;
+    double y = 0;
+    x = (b1 - b2) / (-k1 + k2);
+    y = k2 * x + b2;
+    Console.Write($"Cross coordinate M({x}, {y}) ");
+}
